dabff107dd3d9d79ec14f0d8b8d3de9a53de4179
Peter Kröner <peter@peterkroener.de>
2018-03-01 00:30:36 +0100

    New watching rates for changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const http = require("http");
 const final = require("finalhandler");
 const serveStatic = require("serve-static");
@@ -8,3 +9,7 @@ const serve = serveStatic("./", {
 
 const server = http.createServer( (req, res) => serve(req, res, final(req, res)) );
 server.listen(3000, () => console.log("Server listening @ port 3000") );
+
+fs.watchFile("./api/latest.json", () => {
+  console.log("New rates detected...");
+});
