d03b15ab71f2ab71d54c32b99a6b6f2f53367ed6
Peter Kröner <peter@peterkroener.de>
2018-03-01 00:21:39 +0100

    Added minimal http server

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const final = require("finalhandler");
+const serveStatic = require("serve-static");
+
+const serve = serveStatic("./", {
+  index: [ "index.html" ],
+});
+
+const server = http.createServer( (req, res) => serve(req, res, final(req, res)) );
+server.listen(3000, () => console.log("Server listening @ port 3000") );
